dda41d0bbaca5812f9d47897fb346cbeaa6d6824
Neha Moopen <neha.moopen@gmail.com>
2023-01-13 13:13:14 +0100

    initial commit

---
+++ download_illustrations.R
@@ -0,0 +1,36 @@
+install.packages("zen4R")
+
+require("remotes")
+install_github("eblondel/zen4R")
+
+library(zen4R)
+
+zenodo <- ZenodoManager$new(logger = "INFO")
+zenodo <- ZenodoManager$new()
+
+zenodo <- ZenodoManager$new(
+  token = "PeETiaMQv2lY7ICVfGwSoFQ1zZXilf74eZVenZ02lcAmDYMmS9Lrj8EulsXR", 
+  logger = "INFO" # use "DEBUG" to see detailed API operation logs, use NULL if you don't want logs at all
+)
+
+rec <- zenodo$getRecordByDOI("10.5281/zenodo.6821117")
+files <- rec$listFiles(pretty = TRUE)
+
+#create a folder where to download my files
+dir.create("images")
+
+# drop records with pdfs
+
+library(dplyr)
+library(stringr)
+files <- filter(files, str_detect(filename, "jpg"))
+
+# download files
+
+for(i in seq_len(nrow(files))) {
+  download.file(url = files$download[i], 
+                destfile = file.path("images", files$filename[i]), 
+                method = 'auto')
+}
+
+relative_paths <- list.files("images", full.names = TRUE)
